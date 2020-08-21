536eb10760be30e58a7d70f23730602e7e9b1d4c
SatvikJayalwal <69150647+SatvikJayalwal@users.noreply.github.com>
2020-08-21 16:20:24 +0530

    Add files via upload

---
+++ Apni_dictionary.py
@@ -0,0 +1,6 @@
+import os; os.system("cls")
+dicty={"eagerly":"Exited","longings":"desire","Delightful":"lovely","pious":"Religious"}
+inp=input("Enter the word to be searched : ")
+print("The meaning of ",inp,":",dicty[inp])
+
+
